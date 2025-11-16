960d92173effe93ad6b135cdad772829f4e42a83
shoushan <shoushan@lys.moe>
2025-11-16 15:50:46 -0500

    recent writing

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -40,6 +40,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
+    Component.RecentNotes({ title: "Recent writing" }),
   ],
   left: [
     Component.PageTitle(),
