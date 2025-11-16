c7d5e83c256d65880c4f5f5de010963209136756
shoushan <shoushan@lys.moe>
2025-11-16 15:33:18 -0500

    giscus

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,23 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: "staryes/doppio-espresso",
+        // from data-repo-id
+        repoId: "R_kgDOQWivLw",
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: "DIC_kwDOQWivL84Cx26w",
+      // from data-lang
+      lang: 'en'
+    }
+  }),
+],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/staryes/doppio-espresso",
